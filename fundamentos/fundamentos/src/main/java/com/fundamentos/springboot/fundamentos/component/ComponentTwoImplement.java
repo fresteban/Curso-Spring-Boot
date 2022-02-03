e6ffc20d34a4d999f6770d99d8b76ba3d7389019
Esteban Fredes Reyes <esteban.f.r00@gmail.com>
2022-02-03 18:33:24 -0300

    Fix ejemplo dependencia y componentes

--- fundamentos/fundamentos/src/main/java/com/fundamentos/springboot/fundamentos/component/ComponentTwoImplement.java
+++ fundamentos/fundamentos/src/main/java/com/fundamentos/springboot/fundamentos/component/ComponentTwoImplement.java
@@ -1,2 +1,10 @@
-package com.fundamentos.springboot.fundamentos.component;public class ComponentTwoImplement {
+package com.fundamentos.springboot.fundamentos.component;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentTwoImplement implements ComponentDependency{
+    @Override
+    public void saludar() {
+        System.out.println("Hola mundo desde mi componente dos");
+    }
 }
